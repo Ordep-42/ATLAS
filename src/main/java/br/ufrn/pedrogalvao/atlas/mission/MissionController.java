587b8f1d44ac2dfc51898ab17af079ec342e790d
Pedro Galvão <90785456+Ordep-42@users.noreply.github.com>
2026-06-10 12:11:06 -0300

    chore: Criação do controller.

---
+++ src/main/java/br/ufrn/pedrogalvao/atlas/mission/MissionController.java
@@ -0,0 +1,42 @@
+package br.ufrn.pedrogalvao.atlas.mission;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/missions")
+public class MissionController {
+
+	private final MissionService service;
+	
+	public MissionController(MissionService service) {
+		this.service = service;
+	}
+	
+	@PostMapping
+	public ResponseEntity<Mission> create(@RequestBody Mission mission) {
+		Mission created = service.create(mission.getName(), mission.getDescription());
+		return ResponseEntity.status(HttpStatus.CREATED).body(created);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Mission>> listAll() {
+		return ResponseEntity.ok(service.listAll());
+	}
+
+	@GetMapping("/{id}")
+    public ResponseEntity<Mission> findById(@PathVariable Long id) {
+        return service.findById(id)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<Mission> updateStatus(@PathVariable Long id, @RequestBody MissionStatus status) {
+        Mission updated = service.updateStatus(id, status);
+        return ResponseEntity.ok(updated);
+    }
+}
